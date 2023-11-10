bd82305c85dc059e381f7393fbd21c601051efb7
zeeshanalikhan15 <zeeshana@virtualq.com>
2023-11-11 02:24:31 +0500

    Deploy to GitHub Pages

--- src/components/Intro/IntroSection.jsx
+++
@@ -1,7 +0,0 @@
-export default function MyCustomIntro(){
-    return (
-        <>
-        <p>ye mera cusomter intro component hai <b>bhai!</b></p>
-        </>
-    );
-}
--- src/components/Midro/MidroSection.jsx
+++
@@ -1,8 +0,0 @@
-export default function MyCustomMidro(){
-    return (
-        <>
-        <p>ye mera Midro component hai <b>bhai!</b></p>
-        <p>or mein iss mein new changes dal deni hain</p>
-        </>
-    );
-}
